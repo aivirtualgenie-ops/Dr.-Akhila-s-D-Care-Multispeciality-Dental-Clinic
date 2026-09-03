db058e7ddbb47fbb6b3bd626144682738a4751fd
aivirtualgenie-ops <aivirtualgenie@gmail.com>
2026-09-03 20:00:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+document.getElementById("year").textContent = new Date().getFullYear();
 
+document.querySelector(".menu").addEventListener("click", () => {
+  const nav = document.querySelector("nav");
+
+  nav.style.display = nav.style.display === "flex" ? "" : "flex";
+  nav.style.position = "absolute";
+  nav.style.top = "70px";
+  nav.style.left = "0";
+  nav.style.right = "0";
+  nav.style.padding = "20px 7%";
+  nav.style.background = "#fff";
+  nav.style.flexDirection = "column";
+});
